5940d50f83fc545402b045493945bd08947dfe4e
dimaboga <bogatireff.d@yandex.kz>
2023-11-05 21:10:48 +0600

    Initial Commit

---
+++ Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by Дмитрий Богатырев on 05.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
